99b18904672759300e8d4d708e74544c66feff8c
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-07-21 12:37:32 +0200

    update front end

--- assets/App.vue
+++ assets/App.vue
@@ -1,114 +1,33 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+export default {
+    name: "App",
+
+    components: {
+        Header,
+        Main,
+        Footer,
+    },
+};
 </script>
 
 <template>
-    <header>
-        <div class="wrapper">
-            <HelloWorld msg="You did it!" />
-
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-            </nav>
-        </div>
-    </header>
-    <RouterView />
+    <Header />
+    <Main />
+    <Footer />
 </template>
 
-<style>
-#app {
-    max-width: 1280px;
-    margin: 0 auto;
-    padding: 2rem;
-
-    font-weight: normal;
-}
-
-header {
-    line-height: 1.5;
-    max-height: 100vh;
-}
-
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
-
-a,
-.green {
-    text-decoration: none;
-    color: hsla(160, 100%, 37%, 1);
-    transition: 0.4s;
-}
-
-@media (hover: hover) {
-    a:hover {
-        background-color: hsla(160, 100%, 37%, 0.2);
-    }
-}
-
+<style lang="scss" scoped>
 nav {
     width: 100%;
     font-size: 12px;
-    text-align: center;
     margin-top: 2rem;
-}
 
-nav a.router-link-exact-active {
-    color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-    background-color: transparent;
-}
-
-nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-    border: 0;
-}
-
-@media (min-width: 1024px) {
-    body {
-        display: flex;
-        place-items: center;
-    }
-
-    #app {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        padding: 0 2rem;
-    }
-
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    nav {
-        text-align: left;
-        margin-left: -1rem;
-        font-size: 1rem;
-
-        padding: 1rem 0;
-        margin-top: 1rem;
+    a {
+        margin: 2rem;
     }
 }
 </style>
--- assets/views/HomeView.vue
+++ assets/views/HomeView.vue
@@ -1,9 +1,95 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <div class="main-home d-flex justify-content-center align-items-center">
+            <div class="main-title">
+                <h1>Mercurio</h1>
+                <span class="content-title">content</span>
+                <span class="connect-title">connect</span>
+                <span class="like-title">like</span>
+                <span class="share-title">share</span>
+                <span class="web-title">web</span>
+                <span class="links-title">links</span>
+            </div>
+        </div>
+    </main>
 </template>
+
+<style lang="scss" scoped>
+div.main-home {
+    height: 84.9vh;
+    div.main-title {
+        font-family: "Blaka", cursive;
+        text-transform: uppercase;
+        color: white;
+        position: relative;
+        h1 {
+            font-size: 6rem;
+            animation: title-load 400ms ease-in;
+        }
+
+        span {
+            font-size: 2rem;
+            position: absolute;
+        }
+        span.content-title {
+            top: 40px;
+            left: -140px;
+            animation: left-load 1600ms ease-in;
+        }
+        span.connect-title {
+            top: -30px;
+            left: 100px;
+            animation: left-load 1600ms ease-in;
+        }
+        span.links-title {
+            bottom: -30px;
+            left: 100px;
+            animation: left-load 1600ms ease-in;
+        }
+        span.web-title {
+            bottom: -30px;
+            left: 280px;
+            animation: right-load 1600ms ease-in;
+        }
+        span.share-title {
+            top: 40px;
+            right: -110px;
+            animation: right-load 1600ms ease-in;
+        }
+        span.like-title {
+            top: -30px;
+            left: 280px;
+            animation: right-load 1600ms ease-in;
+        }
+    }
+}
+
+@keyframes title-load {
+    0% {
+        transform: translateX(-300%);
+    }
+    100% {
+        transform: translateX(0);
+    }
+}
+
+@keyframes right-load {
+    0% {
+        transform: translateX(1000%);
+    }
+    100% {
+        transform: translateX(0);
+    }
+}
+
+@keyframes left-load {
+    0% {
+        transform: translateX(-1000%);
+    }
+    100% {
+        transform: translateX(0);
+    }
+}
+</style>
